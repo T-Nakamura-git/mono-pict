994543026402321af257a6573949786747d955ea
T-Nakamura-git <unbreakablechain@gmail.com>
2019-02-07 11:31:05 +0000

    test commit

---
+++ app/controllers/fundamental_controller.rb
@@ -0,0 +1,16 @@
+class FundamentalController < ApplicationController
+    def top
+    end
+    
+    def cats
+    end
+    
+    def dogs
+    end
+    
+    def others
+    end
+    
+    def sitepolicy
+    end    
+end
